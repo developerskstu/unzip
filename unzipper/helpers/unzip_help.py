d491041ce393f676e960b0c9b96d142b9d25ba39
developerssh <119683709+developerssh@users.noreply.github.com>
2023-05-03 21:08:06 +0530

    Update unzip_help.py

--- unzipper/helpers/unzip_help.py
+++ unzipper/helpers/unzip_help.py
@@ -37,7 +37,7 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         )
         try:
             await message.edit(
-                text="{}\n {} \n\n**Powered by @EDM115bots**".format(ud_type, tmp)
+                text="{}\n {} \n\n**📥Download by kshitij **".format(ud_type, tmp)
             )
         except:
             pass
